2816a726f08d3c6d8058e5b6c76d2a2d7ccc26d6
tania <taniaosano15@gmail.com>
2022-08-18 01:08:19 +0300

    Completed control flow lab

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(feet){
   // Write your code here!
-}
+  if(feet <= 400) {
+    return "This one is on me!";
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
+  } else if(feet > 2000 && feet < 2501 ){
+    return "I will gladly take your thirty bucks.";
 
-function switchOnCharmFromTip(){
+  } else if ( feet > 2500) {
+    return "No can do.";
+  }
+
+  }
+
+function ternaryCheckCity(city){
   // Write your code here!
-}
+   return city === 'NYC'? "Ok, sounds good." : "No go.";
+   
+ }
+
+function switchOnCharmFromTip(tip){
+  // Write your code here!
+ switch(tip)
+  {
+    case "generous":
+      return "Thank you so much.";
+      break;
+    case "not as generous":
+      return "Thank you.";
+      break;
+
+    case "thanks for everything":
+      return "Bye.";   
+ }
+ }  
+
